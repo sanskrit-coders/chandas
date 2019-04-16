706c502624d28d26613c56fe8a2061273bcf9365
vvasuki <vishvas.vasuki@gmail.com>
2019-04-16 15:30:09 -0700

    Add identify module

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
   # Versions should comply with PEP440.  For a discussion on single-sourcing
   # the version across setup.py and the project code, see
   # https://packaging.python.org/en/latest/single_source_version.html
-  version='0.0.2',
+  version='0.0.3',
 
   description='Metrical analysis tools for indic languages in general, and sanskrit in particular',
   long_description=long_description,
