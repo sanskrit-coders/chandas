5e24f9d7ff1aca047200e189660c869d1da548b4
vvasuki <vishvas.vasuki@gmail.com>
2020-09-22 17:56:12 +0530

    CU

--- setup.py
+++ setup.py
@@ -22,6 +22,13 @@ try:
 except (IOError, ImportError):
   long_description = ''
 
+with open('requirements.txt', 'r') as f:
+  install_reqs = [
+    s for s in [
+      line.split('#', 1)[0].strip(' \t\n') for line in f
+    ] if s != ''
+  ]
+
 setup(
   name='chandas',
 
@@ -87,7 +94,7 @@ setup(
   # your project is installed. For an analysis of "install_requires" vs pip's
   # requirements files see:
   # https://packaging.python.org/en/latest/requirements.html
-  install_requires=['PyICU', 'unicodecsv', 'indic_transliteration'],
+  install_requires=install_reqs,
 
   # List additional groups of dependencies here (e.g. development
   # dependencies). You can install these using the following syntax,
