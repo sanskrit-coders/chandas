321589188becc6463084e1551e90a34dc19ecda7
Vishvas Vasuki विश्वासः <vishvas.vasuki@gmail.com>
2022-03-03 16:48:10 +0530

    Useful patterns

--- chandas/syllabize.py
+++ chandas/syllabize.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import logging
 import string
+from argparse import ArgumentError
 from itertools import takewhile
 
 import icu
@@ -30,7 +31,8 @@ def is_vyanjanaanta(in_string):
 
 
 def has_vowel(in_string):
-  return bool(regex.fullmatch(".*[ऄ-औ ऺ-ऻ ा-ौ ॎ-ॏ ॲ-ॷ].*".replace(" ", ""), in_string, flags=regex.UNICODE) or regex.match(".*[क-हक़-य़ॸ-ॿ](?=([^्]|$))", in_string, flags=regex.UNICODE))
+  dev_scheme = sanscript.SCHEMES[sanscript.DEVANAGARI]
+  return bool(regex.findall("[%s%s]" % (dev_scheme.PATTERN_INDEPENDENT_VOWEL, dev_scheme.PATTERN_DEPENDENT_VOWEL), in_string, flags=regex.UNICODE) or regex.match(".*[%s](?=([^्]|$))" % (dev_scheme.PATTERN_VYANJANA), in_string, flags=regex.UNICODE))
 
 
 def begins_with_vowel(in_string):
@@ -46,14 +48,21 @@ def get_syllables(in_string):
   """
   # Cannot do \P{Letter} below as it does not match mAtra-s and virAma-s as of 2019.
   # ऀ-़ा-ॣॲ-ॿ
-  cleaned_phrase = regex.sub(r"([^ऀ-़ ा-ॣ ॲ-ॿ  ꣠-ꣽ  ᳐-᳹])", "", in_string, flags=regex.UNICODE)
-  cleaned_phrase = cleaned_phrase.replace("ॐ", "ओम्")
+
+  dev_scheme = sanscript.SCHEMES[sanscript.DEVANAGARI]
+  cleaned_phrase  = in_string
+  cleaned_phrase = regex.sub("[%s]" % dev_scheme.PATTERN_OM, "ओम्", cleaned_phrase)
+  cleaned_phrase = regex.sub(r"([^%s%s%s%s%s%s])" % (dev_scheme.PATTERN_INDEPENDENT_VOWEL, dev_scheme.PATTERN_VYANJANA, dev_scheme.PATTERN_DEPENDENT_VOWEL, dev_scheme.PATTERN_YOGAVAAHA, dev_scheme.PATTERN_ACCENT, dev_scheme.PATTERN_CONSONANT_MODIFIER), "", cleaned_phrase, flags=regex.UNICODE)
   cleaned_phrase = cleaned_phrase.replace(" ", "")
-  cleaned_phrase = regex.sub(r"([क-हक़-य़ॸ-ॿ]्)([ऄ-औॠॡॲ-ॷ])", lambda x: sanscript.SCHEMES[sanscript.DEVANAGARI].do_vyanjana_svara_join(x.group(1), x.group(2)), cleaned_phrase)
+  cleaned_phrase = regex.sub(r"(%s)([%s])" % (dev_scheme.PATTERN_VYANJANA_WITHOUT_VOWEL, dev_scheme.PATTERN_INDEPENDENT_VOWEL), lambda x: sanscript.SCHEMES[sanscript.DEVANAGARI].do_vyanjana_svara_join(x.group(1), x.group(2)), cleaned_phrase)
   syllables = []
   while len(cleaned_phrase) > 0:
     # possible vyanjanas without vowels + svara or vyanjana + possible vowel marks + possible yogavAhas + possible accents + possible vyanjanas without vowels
-    match = regex.match(r"([क-हक़-य़ॸ-ॿ]्)*[ऄ-हक़-ॡॲ-ॿ][ऺ-ॏॢ-ॣॕ-ॗ]*[ऀ-ः]*[ ꣠-ꣽ  ᳐-᳹]*([क-हक़-य़ॸ-ॿ]्ँ?)*", cleaned_phrase)
+    match = regex.match((r"(%s)*[%s%s][%s]*[%s]*[%s]*(%sँ?)*" % (dev_scheme.PATTERN_VYANJANA_WITHOUT_VOWEL,dev_scheme.PATTERN_INDEPENDENT_VOWEL, dev_scheme.PATTERN_VYANJANA, dev_scheme.PATTERN_DEPENDENT_VOWEL, dev_scheme.PATTERN_YOGAVAAHA, dev_scheme.PATTERN_ACCENT, dev_scheme.PATTERN_VYANJANA_WITHOUT_VOWEL, )), cleaned_phrase)
+    if match is None:
+      message = "No match! Input - %s Remaining - %s" % (in_string, cleaned_phrase)
+      logging.fatal(message)
+      raise ValueError(message)
     current_syllable = match.group(0)
     cleaned_phrase = cleaned_phrase.replace(match.group(0), "", 1)
     if len(current_syllable) > 0:
@@ -62,9 +71,10 @@ def get_syllables(in_string):
 
 
 def get_syllable_weight(syllable):
-  if regex.match(r"([क-हक़-य़ॸ-ॿ]्)*[क-हक़-य़ॸ-ॿ][ ऻ ा ी ू ॄ ॗॣ ॎ े ै ो ौ ॕ ं ः  ᳢-ᳬ ᳮ ᳯ ᳰ ᳱ ᳲ ᳳ ᳵ ᳶ]".replace(" ", ""), syllable):
+  dev_scheme = sanscript.SCHEMES[sanscript.DEVANAGARI]
+  if regex.findall(r"[%s%s%s]" % (dev_scheme.PATTERN_GURU_INDEPENDENT_VOWEL, dev_scheme.PATTERN_GURU_DEPENDENT_VOWEL, dev_scheme.PATTERN_GURU_YOGAVAAHA), syllable):
     return "G"
-  elif regex.findall(r"[ऄ-हक़-ॡॲ-ॿ][ऺ-ॏॢ-ॣॕ-ॗ]*[ऀ-ः]*[ ꣠-ꣽ  ᳐-᳹]*([क-हक़-य़ॸ-ॿ]्ँ?)+", syllable):
+  elif regex.findall(r"[%s%s][%s]*[%s]*[%s]*(%s)+" % (dev_scheme.PATTERN_INDEPENDENT_VOWEL, dev_scheme.PATTERN_VYANJANA, dev_scheme.PATTERN_DEPENDENT_VOWEL, dev_scheme.PATTERN_YOGAVAAHA, dev_scheme.PATTERN_ACCENT, dev_scheme.PATTERN_VYANJANA_WITHOUT_VOWEL, ), syllable):
     return "G"
   else:
     return "L"
